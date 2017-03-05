42b99fa68d3d7530dabe54fee340b4689f5ebe9c
John Pillar <johnpillar@Johns-MacBook-Pro.local>
2017-03-05 07:53:13 +0000

    Made sure bing-wallpaper folder exists

--- bingwallpaper.js
+++ bingwallpaper.js
@@ -1,27 +1,49 @@
-var request = require('request'),
-    fs      = require('fs');
+var request  = require('request'),
+    fs       = require('fs'),
+    filepath = '/users/johnpillar/Pictures/bing-wallpaper/';
 
-request('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-gb', function (error, response, data) {
-    if (!error && response.statusCode == 200) {
+function prepfolderlocation (result) {
+    fs.stat(filepath, function (err, stats) {
+        if (err) {
+            // Directory doesn't exist or some other error
+            console.log('Folder doesn\'t exist, so I made the folder: ' + filepath);
+            fs.mkdir(filepath);
+        };
+    });
+};
 
-        var jsonObj = JSON.parse(data),
-            imageUrl = 'https://www.bing.com/' + jsonObj.images[0].url;
+function downloadwallpaper (result) {
+    request('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-gb', function (error, response, data) {
+        if (!error && response.statusCode == 200) {
 
-        // Get higher res image
-        imageUrl = imageUrl.replace('1080','1200');
-        console.log ('Getting image: ' + imageUrl);
+            var jsonObj = JSON.parse(data),
+                imageUrl = 'https://www.bing.com/' + jsonObj.images[0].url;
 
-        request(imageUrl, {encoding: 'binary'}, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+            // Get higher res image
+            imageUrl = imageUrl.replace('1080','1200');
+            console.log ('Getting image: ' + imageUrl);
 
-                fs.writeFile('downloaded.jpg', body, 'binary', function (err) {});
+            request(imageUrl, {encoding: 'binary'}, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
 
-            } else {
-                console.log ('Error getting bing wallpaper image.');
-            }
-        });
+                    // Construct filename
+                    var filename = imageUrl.replace(/^.*[\\\/]/, '');
+                    
+                    // Save file
+                    fs.writeFile(filepath + filename, body, 'binary', function (err) {});
+                    console.log ('Downloaded and saved today\'s bing wallpaper.');
 
-    } else {
-        console.log ('Error getting bing wallpaper data.'); 
-    };
-});
+                } else {
+                    console.log ('Error getting bing wallpaper image.');
+                }
+            });
+
+        } else {
+            console.log ('Error getting bing wallpaper data.'); 
+        };
+    });
+    return true;
+};
+
+prepfolderlocation();
+downloadwallpaper ();
