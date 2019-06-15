ecb9977a4e6c01cf7d612120a01a9edef33d6ad4
JOHNEPPILLAR <johneppillar@gmail.com>
2019-06-15 16:52:13 +0100

    Updated deps and added eslint

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
-# bing-wallpaper-for-mac
+# BingWallpaper
+
 Daily Bing wallpaper for mac
 
 ###Notes
--  In order to save space, the script assumes that *only 1 image* is kept in the ~/Pictures/bing-wallpaper folder.
--  Place all the files into a single folder. Here it is in the Applications folder within the user's home directory - `~/Applications/BingWallpaper`
--  Open the plist file with your favorite editor and change the username to your username
+
+- In order to save space, the script assumes that _only 1 image_ is kept in the ~/Pictures/bing folder.
+- Place all the files into a single folder. Here it is in the Applications folder within the user's home directory - `~/Applications/BingWallpaper`
+- Open the plist file with your favorite editor and change the username to your username
 
 ###Plist File
+
 - Copy the plist file to `~/Library/LaunchAgents`
+
 ```
 cp bing.wallpaper.mac.plist ~/Library/LaunchAgents
 ```
+
 - Edit the plist file to reflect your username
 - Save and close
 - Run the following terminal command
+
 ```
 launchctl load ~/Library/LaunchAgents/bing.wallpaper.mac.plist
 ```
 
 ###Uninstall
+
 - To uninstall run this command
+
 ```
 launchctl unload ~/Library/LaunchAgents/bing.wallpaper.mac.plist
 ```
+
 - Delete the `~/Applications/BingWallpaper` folder and its contents
-- Delete the `~/Pictures/bing-wallpaper` folder and its contents
+- Delete the `~/Pictures/bing` folder and its contents
--- bingwallpaper.js
+++ bingwallpaper.js
@@ -1,89 +1,80 @@
-var request    = require('request'),
-    fs         = require('fs'),
-    folderpath = '/users/johnpillar/Pictures/bing-wallpaper';
+/* eslint-disable no-console */
+
+const request = require('request');
+const fs = require('fs');
+const dns = require('dns');
+
+const folderpath = '/users/jp/Pictures/bing';
 
 function checkInternet(cb) {
-    require('dns').lookup('google.com',function(err) {
-        if (err && err.code == "ENOTFOUND") {
-            cb(false);
-        } else {
-            cb(true);
-        }
-    })
+  dns.lookup('google.com', (err) => {
+    if (err && err.code === 'ENOTFOUND') {
+      console.log('Not online');
+      cb(false);
+    }
+    cb(true);
+  });
 }
 
-function rmDir (dirPath, removeSelf) {
-    if (removeSelf === undefined)
-        removeSelf = true;
-    try { var files = fs.readdirSync(dirPath); }
-    catch(e) { return; }
-    if (files.length > 0)
-        for (var i = 0; i < files.length; i++) {
-            var filePath = dirPath + '/' + files[i];
-            if (fs.statSync(filePath).isFile())
-                fs.unlinkSync(filePath);
-            else
-                rmDir(filePath);
-            }
-    if (removeSelf)
-        fs.rmdirSync(dirPath);
-};
-
-function prepfolderlocation (dirpath, result) {
-
-    // Remove folder contents
-    console.log ('Removing contents of folder: ' + dirpath);
-    rmDir (folderpath, false);
-
-    // If folder does not exist create it
-    fs.stat(dirpath, function (err, stats) {
-        if (err) {
-            // Directory doesn't exist or some other error
-            console.log('Folder doesn\'t exist, so I made the folder: ' + dirpath);
-            fs.mkdir(dirpath);
-        };
-    });
-};
-
-function downloadwallpaper (result) {
-    request('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-gb', function (error, response, data) {
-        if (!error && response.statusCode == 200) {
-
-            var jsonObj = JSON.parse(data),
-                imageUrl = 'https://www.bing.com' + jsonObj.images[0].url;
-
-            // Get higher res image
-            //imageUrl = imageUrl.replace('1080','1200'); // Removed as not all images are in SHD
-            console.log ('Getting image: ' + imageUrl);
-
-            request(imageUrl, {encoding: 'binary'}, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-
-                    // Construct filename
-                    var filename = imageUrl.replace(/^.*[\\\/]/, '');
-
-                    // Save file
-                    fs.writeFile(folderpath + '/' + filename, body, 'binary', function (err) {});
-                    console.log ('Downloaded and saved today\'s bing wallpaper.');
-
-                } else {
-                    console.log ('Error getting bing wallpaper image.');
-                }
-            });
-
-        } else {
-            console.log ('Error getting bing wallpaper data.'); 
-        };
-    });
-    return true;
-};
-
-checkInternet(function(isConnected) {
-    if (isConnected) {
-        // connected to the internet
-        prepfolderlocation(folderpath);
-        downloadwallpaper ();
-    } else {
-        // not connected to the internet so do nothing
+function rmDir(dirPath, removeSelf) {
+  let files;
+  // eslint-disable-next-line no-param-reassign
+  if (removeSelf === undefined) removeSelf = true;
+  try {
+    files = fs.readdirSync(dirPath);
+  } catch (e) {
+    return;
+  }
+  if (files.length > 0) {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < files.length; i++) {
+      const filePath = `${dirPath}/${files[i]}`;
+      if (fs.statSync(filePath).isFile()) fs.unlinkSync(filePath);
+      else rmDir(filePath);
     }
+  }
+  if (removeSelf) fs.rmdirSync(dirPath);
+}
+
+function prepFolderLocation(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    // Remove folder contents
+    console.log(`Removing contents of folder: ${dirPath}`);
+    rmDir(folderpath, false);
+  } else {
+    console.log(`Folder doesn't exist, so I made the folder: ${dirPath}`);
+    fs.mkdirSync(dirPath);
+  }
+}
+
+function downloadWallpaper() {
+  request(
+    'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-gb',
+    (imageDataerror, imageDataresponse, data) => {
+      if (!imageDataerror && imageDataresponse.statusCode === 200) {
+        const jsonObj = JSON.parse(data);
+        const imageUrl = `https://www.bing.com${jsonObj.images[0].url}`;
+        request(imageUrl, { encoding: 'binary' }, (imageError, imageResponse, body) => {
+          if (!imageError && imageResponse.statusCode === 200) {
+            // Save image
+            const filename = imageUrl.replace(/^.*[\\\/]/, '');
+            fs.writeFile(`${folderpath}/${filename}`, body, 'binary', () => {});
+            console.log("Downloaded and saved today's bing wallpaper.");
+          } else {
+            console.log('Error getting bing wallpaper image.');
+          }
+        });
+      } else {
+        console.log('Error getting bing wallpaper data.');
+      }
+    },
+  );
+  return true;
+}
+
+checkInternet((isConnected) => {
+  if (isConnected) {
+    prepFolderLocation(folderpath);
+    downloadWallpaper();
+  }
 });
